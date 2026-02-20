a61b505839429865bd03ffea35ab7e30ddcc1aa6
Rishab A <62881970+rishab0406@users.noreply.github.com>
2026-02-19 22:24:23 -0500

    Time: 1 ms (66.57%), Space: 19.4 MB (19.93%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+        for ch in s + t:
+            result = result ^ ord(ch)
+
+        return chr(result)
